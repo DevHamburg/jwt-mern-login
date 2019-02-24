c07a5e6d2955fd5d78762d740a510263cc4b5db6
DevHamburg <guberinic040@gmail.com>
2019-02-24 11:36:21 +0100

    Add passport JWT authentication strategy

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const keys = require('../config/keys')
+const passport = require('passport')
 
 const User = require('../models/User')
 
@@ -63,5 +64,17 @@ router.post('/login', (req, res) => {
     })
   })
 })
+
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+    })
+  }
+)
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const users = require('./routes/users')
 
@@ -16,7 +17,9 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello'))
+app.use(passport.initialize())
+
+require('./config/passport')(passport)
 
 app.use('/users', users)
 
