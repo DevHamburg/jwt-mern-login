4a8192696e3c49c92d1f28dc2bf65428f1c33f7d
DevHamburg <guberinic040@gmail.com>
2019-02-25 09:44:43 +0100

    Add react app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Navigation from './components/Navigation'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Navigation />
+      </>
+    )
+  }
+}
+
+export default App
